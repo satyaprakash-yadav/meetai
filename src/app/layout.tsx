4b78b81b41e3d415066c316673ec863415afa23f
satyaprakash-yadav <satyay9798@gmail.com>
2025-06-07 13:17:16 +0530

    11: agents form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Toaster } from "@/components/ui/sonner";
 import { TRPCReactProvider } from "@/trpc/client";
 
 const inter = Inter({
@@ -24,6 +25,7 @@ export default function RootLayout({
         <body
           className={`${inter.className} antialiased`}
         >
+          <Toaster />
           {children}
         </body>
       </html>
