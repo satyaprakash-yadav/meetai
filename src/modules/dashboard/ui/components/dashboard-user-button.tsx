a976106fb37dd1085c773d3f121654f2a99ce3ae
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-07-10 17:43:38 +0530

    27: payments

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -72,7 +72,7 @@ export const DashboardUserButton = () => {
             <DrawerDescription>{data.user.email}</DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
-            <Button variant="outline" onClick={() => {}}>
+            <Button variant="outline" onClick={() => authClient.customer.portal()}>
               <CreditCardIcon className="size-4 text-black" />
               Billing
             </Button>
@@ -116,7 +116,10 @@ export const DashboardUserButton = () => {
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+        <DropdownMenuItem
+          onClick={() => authClient.customer.portal()}
+          className="cursor-pointer flex items-center justify-between"
+        >
           Billing
           <CreditCardIcon className="size-4" />
         </DropdownMenuItem>
